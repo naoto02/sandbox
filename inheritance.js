3e617a371ee94ce0673fb28987a5e4dd26479ce0
Naoto Igarashi <naoto.stb@gmail.com>
2015-08-08 19:18:39 +0900

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,25 +1,26 @@
 // INHERITANCE: Pattern1
 
-console.log("INHERITANCE pattern1");
+console.log("=====INHERITANCE pattern1=====");
 var Parent = function(name, age) {
 	this.name = name;
 	this.age = age;
 	console.log("Parent constructor");
 };
 
-Parent.prototype = {
-	getName: function() {
+// Defne methods
+Parent.prototype.getName = function() {
 		return this.name;
-	},
-	getAge : function() {
-		return this.age;
-	},
-	showData: function() {
-		console.log("Parent: showData");
-	}
-
 };
 
+Parent.prototype.getAge = function() {
+		return this.age;
+};
+
+Parent.prototype.showData = function() {
+		console.log("Parent: showData");
+};
+
+// Child class
 var Child = function() {
 	Parent.apply(this, arguments);
 };
@@ -35,6 +36,7 @@ Child.prototype = {
 	}
 };
 
+// Create objects
 console.log("\nCreate john");
 var john = new Parent("john", 27);
 john.showData();
@@ -46,44 +48,44 @@ daniel.showData();
 
 
 // INHERITANCE: Pattern2
-console.log("\nINHERITANCE Pattern2");
+console.log("\n=====INHERITANCE Pattern2=====");
 
+// Animal class
 var Animal = function(name, weight) {
 	this.name = name;
 	this.weight = weight;
 	console.log("Animal constructor");
 };
-Animal.prototype = {
-	showData: function() {
+Animal.prototype.showData = function() {
 		console.log("Animal: showData");
-	},
-	getName: function() {
+};
+
+Animal.prototype.getName = function() {
 		return this.name;
-	}
 };
 
 var Cat = function() {
 	Animal.apply(this, arguments);
 };
 
+// Cat class
 Cat.prototype = Object.create(Animal.prototype);
 Cat.prototype.constructor = Cat;
 
-Cat.prototype = {
-	showData: function() {
+Cat.prototype.showData = function() {
 		console.log("Cat: showData");
-	},
-	getName: function() {
-		return this.name;
-	}
 };
 
-console.log("\nCreate bob");
+Cat.prototype.getName = function() {
+		return this.name;
+};
+
+console.log("Create bob\n");
 var bob = new Animal("bob", 10.9);
 console.log(bob.getName());
 bob.showData();
 
-console.log("\nCreate mie");
+console.log("Create mie\n");
 var mie = new Cat("mie", 3.5);
 console.log(mie.getName());
 mie.showData();
